ea9f9f11f92f96a931984b35cf8c31dfc8db8cfa
Mohammad Waliul Islam Khan <techgeneenterprise@gmail.com>
2023-02-04 22:52:04 +0600

    Update fetch url

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', { 
+  const response = await fetch('https://chatai-o3oa.onrender.com/', { 
     method: 'POST',
     headers: {
       'content-type': 'application/json'
